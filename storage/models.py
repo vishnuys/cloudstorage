900354f8a6ec52505b1da830ccb8ef56388e7f64
Vishnu Y S <ysv8152@gmail.com>
2019-10-28 15:16:19 +0530

    Create files database

--- storage/models.py
+++ storage/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
+
 # Create your models here.
+class File(models.Model):
+    version = models.IntegerField()
+    filename = models.CharField(max_length=200)
+    filepath = models.CharField(max_length=400)
+    date_created = models.DateTimeField(auto_now_add=True)
+    last_modified = models.DateTimeField(auto_now=True)
