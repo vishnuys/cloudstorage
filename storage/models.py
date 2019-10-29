891e6f69d268afb1e782ae2d3c3a759c9d1d882c
Vishnu Y S <ysv8152@gmail.com>
2019-10-29 14:01:15 +0530

    bucket creation function

--- storage/models.py
+++ storage/models.py
@@ -4,7 +4,7 @@ from django.db import models
 # Create your models here.
 class File(models.Model):
     version = models.IntegerField()
-    name = models.CharField(max_length=200)
+    name = models.CharField(max_length=200, unique=True)
     bucket = models.ForeignKey('Bucket', on_delete=models.CASCADE)
     date_created = models.DateTimeField(auto_now_add=True)
     last_modified = models.DateTimeField(auto_now=True)
@@ -12,6 +12,6 @@ class File(models.Model):
 
 class Bucket(models.Model):
     version = models.IntegerField()
-    name = models.CharField(max_length=200)
+    name = models.CharField(max_length=200, unique=True)
     date_created = models.DateTimeField(auto_now_add=True)
     last_modified = models.DateTimeField(auto_now=True)
--- storage/views.py
+++ storage/views.py
@@ -1,5 +1,8 @@
-from django.shortcuts import render
+import os
+# from django.shortcuts import render
 from django.http import HttpResponse
+from cloud.settings import ARCHIVE_DIR
+from .models import Bucket
 from django.views.generic import TemplateView
 from django.utils.decorators import method_decorator
 from django.views.decorators.csrf import csrf_exempt
@@ -10,3 +13,19 @@ class ServerStatus(TemplateView):
 
     def get(self, request):
         return HttpResponse('OK')
+
+
+@method_decorator(csrf_exempt, name='dispatch')
+class CreateBucket(TemplateView):
+
+    def post(self, request):
+        name = request.POST.get('name')
+        path = os.path.join(ARCHIVE_DIR, name)
+        result = ""
+        buckets = Bucket.objects.filter(name=name)
+        if len(buckets) == 0:
+            bucket = Bucket(version=1, name=name)
+            bucket.save()
+            os.makedirs(path)
+            result = "Bucket Creation Successful"
+        return HttpResponse(result)
